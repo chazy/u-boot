81778222949fec2a7e73c5c624870f3d3d16f966
Christina Warren <cawarren@ti.com>
2012-03-03 20:41:38 -0600

    booti: Add booti to boot Android from eMMC
    This patch adds support for do_booti() which can read
    an Android boot image from eMMC (orSD) and boot it.
    
    Signed-off-by: Christina Warren <cawarren@ti.com>
    Signed-off-by: Regis Goupil <x176713@ti.com>
    Change-Id: I5120dc728fb6c20490064da562d2abdd3a062b1d

--- arch/arm/lib/bootm.c
+++ arch/arm/lib/bootm.c
@@ -30,6 +30,11 @@
 #include <libfdt.h>
 #include <fdt_support.h>
 
+#include <android_image.h>
+#include <mmc.h>
+#include <asm/omap_common.h>
+#include <asm/arch/mmc_host_def.h>
+
 DECLARE_GLOBAL_DATA_PTR;
 
 #if defined (CONFIG_SETUP_MEMORY_TAGS) || \
@@ -57,6 +62,10 @@ static ulong get_sp(void);
 #if defined(CONFIG_OF_LIBFDT)
 static int bootm_linux_fdt(int machid, bootm_headers_t *images);
 #endif
+
+int do_booti(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[]);
+static u64 get_start_sector(unsigned char *sector, char *ptn);
+static u64 get_partition(struct mmc *mmc, char *ptn);
 
 void arch_lmb_reserve(struct lmb *lmb)
 {
@@ -216,6 +225,266 @@ static int bootm_linux_fdt(int machid, bootm_headers_t *images)
 	return 1;
 }
 #endif
+
+static u64 get_start_sector(unsigned char *sector, char *ptn)
+{
+
+	unsigned char name[72];
+	int i, n;
+	u64 start = 0;
+
+	typedef struct {
+		unsigned char type_uuid[16];
+		unsigned char uniq_uuid[16];
+		unsigned long long first_lba;
+		unsigned long long last_lba;
+		unsigned long long attr;
+		unsigned short name[36];
+	} efi_part_desc_t;
+
+	efi_part_desc_t *entries = (efi_part_desc_t *) sector;
+
+	for (i = 0; i < 4; i++, entries++) {
+		n = 0;
+		/* copying a simple unicode partition name */
+		while (n < (sizeof(entries->name)-1)) {
+			name[n] = entries->name[n];
+			n++;
+		}
+
+		debug("booti: Name found %s\n", name);
+		if (!strcmp((char *)name, ptn)) {
+			start = entries->first_lba;
+			break;
+		}
+	}
+
+	return start;
+}
+
+static u64 get_partition(struct mmc *mmc, char *ptn)
+{
+	u64 start_sector = 0;
+	unsigned char buffer[512];
+	ulong block_count;
+	int j;
+
+	block_count = mmc->block_dev.block_read(1, 0, 1, &buffer);
+	if (block_count == 0) {
+		printf("booti: unable to read the MBR\n");
+		return 0;
+	}
+
+	if (buffer[0x1fe] == 0x55 && buffer[0x1ff] == 0xaa) {
+		debug("booti: MBR signature found\n");
+	} else {
+		printf("booti: MBR signature not found\n");
+		return 0;
+	}
+
+	block_count = mmc->block_dev.block_read(1, 1, 1, &buffer);
+	if (block_count == 0) {
+		printf("booti: error reading primary GPT header\n");
+		return 0;
+	}
+
+	if (memcmp(buffer, "EFI PART", 8)) {
+		printf("booti: efi partition table not found\n");
+		return 0;
+	}
+
+	/* searching for the boot partition */
+	for (j = 2; j < 34; j++) {
+		block_count = mmc->block_dev.block_read(1, j, 1, &buffer);
+		if (block_count == 0) {
+			printf("booti: error reading partition entries %d\n",
+									j);
+			return 0;
+		}
+
+		start_sector = get_start_sector(buffer, ptn);
+		debug("booti: start sector %lld\n", start_sector);
+		if (start_sector != 0)
+			break;
+
+	}
+	return start_sector;
+}
+
+#define ALIGNPAGES(n, pagesz) ((n + (pagesz - 1)) & (~(pagesz - 1)))
+
+int do_booti(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
+{
+	int err;
+	unsigned address = 0;
+	int mmc_controller = -1;
+	char *ptn = "boot";
+	unsigned char boothdr[512];
+	struct andr_img_hdr *hdr = (void *) boothdr;
+	bd_t *bd = gd->bd;
+	ulong initrd_start, initrd_end;
+	void (*theKernel)(int zero, int arch, uint params);
+
+	if (argc < 2)
+		return 1;
+
+	if (!strcmp(argv[1], "mmc0"))
+		mmc_controller = 0;
+	else if (!strcmp(argv[1], "mmc1"))
+		mmc_controller = 1;
+	else
+		address = simple_strtoul(argv[1], NULL, 16);
+
+	if (argc > 2)
+		ptn = argv[2];
+
+	if (mmc_controller != -1) {
+
+		ulong block_count;
+		unsigned sector;
+		u64 start_sector = 0;
+		struct mmc *mmc;
+
+		mmc = find_mmc_device(mmc_controller);
+		if (!mmc) {
+			printf("booti: mmc device not found!!\n");
+			return 1;
+		}
+
+		err = mmc_init(mmc);
+		if (err) {
+			printf("booti: mmc init failed: err - %d\n", err);
+			return 1;
+		}
+
+		/* allows the user to boot from recovery partition
+		 but keep boot partition as the default */
+		start_sector = get_partition(mmc, ptn);
+		if (start_sector == 0) {
+			printf("booti: Unable to find the boot partition\n");
+			return 1;
+		}
+
+		/*
+		reading from controller 1 and sector found earlier
+		reading first sector, and storing it in *buffer
+		*/
+		block_count = mmc->block_dev.block_read(1, start_sector, 1,
+								(void *)hdr);
+		if (block_count == 0) {
+			printf("booti: error loading boot image\n");
+			return 1;
+		}
+
+		if (memcmp(hdr->magic, ANDR_BOOT_MAGIC, 8)) {
+			printf("booti: bad boot image magic\n");
+			return 1;
+		}
+
+		debug("booti: hdr->magic is = %s\n", hdr->magic);
+		debug("booti: kernel addr and kern size = %u %d \n",
+				hdr->kernel_addr, hdr->kernel_size);
+		debug("booti: ramdisk addr and ramdisk size = %u %d \n",
+				hdr->ramdisk_addr, hdr->ramdisk_size);
+
+		sector = start_sector + (hdr->page_size / 512);
+
+		/*
+		reading from controller 1, start address = sector,
+		reading kernel size and storing in kernel addr
+		*/
+		block_count = mmc->block_dev.block_read(1, sector,
+		(hdr->kernel_size/512 + 1), (void *) hdr->kernel_addr);
+		if (block_count == 0) {
+			printf("booti: failed to load kernel : "
+						"mmc blk read err\n");
+			return 1;
+		}
+
+		sector += ALIGNPAGES(hdr->kernel_size, hdr->page_size) / 512;
+
+		block_count = mmc->block_dev.block_read(1, sector,
+		(hdr->ramdisk_size/512 + 1), (void *) hdr->ramdisk_addr);
+		if (block_count == 0) {
+			printf("booti: failed to read ramdisk : "
+						"mmc blk read err\n");
+			return 1;
+		}
+
+	} else {
+		unsigned kernel_address;
+		unsigned ramdisk_address;
+
+		/* copy the header from the bootimg */
+		memcpy(hdr, (void *) address, sizeof(*hdr));
+
+		if (memcmp(hdr->magic, ANDR_BOOT_MAGIC, 8)) {
+			printf("booti: bad boot image magic\n");
+			return 1;
+		}
+
+		debug("booti: hdr->magic is = %s\n", hdr->magic);
+		debug("booti: kernel addr and kern size = %u %d \n",
+				hdr->kernel_addr, hdr->kernel_size);
+		debug("booti: ramdisk addr and ramdisk size = %u %d \n",
+				hdr->ramdisk_addr, hdr->ramdisk_size);
+
+		kernel_address = address + hdr->page_size;
+		ramdisk_address = kernel_address + ALIGN(hdr->kernel_size,
+							hdr->page_size);
+
+		memmove((void *) hdr->kernel_addr,  (void *) kernel_address,
+							hdr->kernel_size);
+		memmove((void *) hdr->ramdisk_addr, (void *) ramdisk_address,
+							hdr->ramdisk_size);
+	}
+
+	printf("booti: kernel   @ %08x (%d)\n", hdr->kernel_addr,
+							hdr->kernel_size);
+	printf("booti: ramdisk  @ %08x (%d)\n", hdr->ramdisk_addr,
+							hdr->ramdisk_size);
+
+	theKernel = (void (*)(int, int, uint))(hdr->kernel_addr);
+
+	initrd_start = hdr->ramdisk_addr;
+	initrd_end = initrd_start + hdr->ramdisk_size;
+
+	#ifdef CONFIG_SETUP_MEMORY_TAGS
+	setup_start_tag(bd);
+	#ifdef CONFIG_SERIAL_TAG
+	setup_serial_tag(&params);
+	#endif
+	#ifdef CONFIG_REVISION_TAG
+	setup_revision_tag(&params);
+	#endif
+	#ifdef CONFIG_SETUP_MEMORY_TAGS
+	setup_memory_tags(bd);
+	#endif
+	#ifdef CONFIG_CMDLINE_TAG
+	setup_commandline_tag(bd, hdr->cmdline);
+	#endif
+	#ifdef CONFIG_INITRD_TAG
+	if (hdr->ramdisk_size)
+		setup_initrd_tag(bd, initrd_start, initrd_end);
+	#endif
+	setup_end_tag(bd);
+	#endif
+
+	printf("\nStarting kernel ...\n\n");
+
+	cleanup_before_linux();
+
+	theKernel(0, bd->bi_arch_number, bd->bi_boot_params);
+
+	return 1;
+}
+
+U_BOOT_CMD(
+	booti,  3,      1,      do_booti,
+	"booti   - boot android bootimg from memory\n",
+	"<addr>\n    - boot application image stored in memory\n"
+	"\t'addr' - address of boot image which contains zImage + ramdisk\n"
+);
 
 #if defined (CONFIG_SETUP_MEMORY_TAGS) || \
     defined (CONFIG_CMDLINE_TAG) || \
